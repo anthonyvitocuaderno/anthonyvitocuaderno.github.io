c34dd54f95821e5cd4339955ef6673ad7ea227fc
Vito Cuaderno <vito@tirsolutions.com>
2018-06-29 17:24:21 +0800

    animation

--- js/assets/splash.js
+++ js/assets/splash.js
@@ -1,11 +1,49 @@
-let constants = {
-	splashDuration: 15000
+var splash = {
+	constants: {
+		splashDuration: 5000,
+		quoteDelay: 1500,
+		typewriterDelay: 300
+	},
+	animateQuote: function() {
+		$('.splash .coffee .coffee-fill').addClass('full')
+		setTimeout(function() {
+			$('.splash .coffee .quote, .splash .coffee .quote-part-2').addClass('smoke-up')
+		}, 250)
+		const quoteLabel = $('.splash .coffee .quote>.text')
+		const quoteLabel2 = $('.splash .coffee .quote-part-2>.text')
+		const messageString = quoteLabel.text()
+		const messageString2 = quoteLabel2.text()
+		quoteLabel.text('')
+		quoteLabel2.html('')
+		setTimeout(function() {
+			var typewriterAnimation = setInterval(function() {
+				var slicedMessageString = messageString.slice(0, quoteLabel.text().length + 1)
+
+				quoteLabel.html(slicedMessageString)
+				if (slicedMessageString.length >= messageString.length) {
+					
+
+					slicedMessageString = messageString2.slice(0, quoteLabel2.text().length + 1)
+
+					quoteLabel2.html(slicedMessageString)
+
+					if (slicedMessageString.length >= messageString2.length) {
+						clearInterval(typewriterAnimation)
+					}
+				}
+			}, splash.constants.typewriterDelay / 5)
+		}, splash.constants.quoteDelay)
+	}
 }
 
 $(document).ready(function () {
-	$('.splash .coffee .coffee-fill').addClass('full')
+
+	
+
+	splash.animateQuote()
+	
 
 	setTimeout(function() {
 		$('.splash').addClass('end')
-	}, constants.splashDuration)
+	}, splash.constants.splashDuration)
 })
