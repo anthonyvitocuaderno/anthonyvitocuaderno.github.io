9817a6877b734fd1526de050dd9e1e2aa2f6298f
Vito Cuaderno <vito@tirsolutions.com>
2018-08-01 14:33:53 +0800

    added skills

--- js/app.js
+++ js/app.js
@@ -22,9 +22,23 @@ $(document).ready(function() {
 	})
 
 	function gotoPage(page) {
+		$('section.summary.active.scrolled').removeClass('scrolled')
+		$('html').animate({
+            scrollTop: 0}, 1000);
 		$('section.active').removeClass('active')
 		$('section.' + page).addClass('active')
 	}
+	document.addEventListener('scroll', function (event) {
+	    $('section.summary.active').addClass('scrolled')
+	    const scrollTop = $('html').scrollTop()
+	    const docHeight = $( document ).height();
+	    if (scrollTop > 0) {
+	    	$('section.summary.active').addClass('scrolled')
+	    } else {
+	    	$('section.summary.active.scrolled').removeClass('scrolled')
+	    }
+
+	}, true);
 })
 
 
